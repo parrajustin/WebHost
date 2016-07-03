59d9e05ee6238769146b7f99d3b285bfe3b925b2
Xero Studios <parra.justin.main@gmail.com>
2016-07-02 23:16:53 -0600

    index.js update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('/WebHome/index.html');
+  res.send('./WebHome/index.html');
 });
 
 app.listen(80);
