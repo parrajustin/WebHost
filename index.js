5d0ce60aad9835ea44c65a249dbbdb8b28a76d9d
Xero Studios <parra.justin.main@gmail.com>
2016-07-03 13:48:07 -0600

    Added interpreter to index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var express = require('express');
 var http = require('http');
 var https = require('https');
@@ -7,11 +8,15 @@ var fs = require('fs');
 ///////////////////////////////////////////////////////
 
 app.all('*', function(req, res, next){
-  if (req.secure) {
-    return next();
-  };
+  // 	if (req.secure) {
+		// return next();
+  // 	};
+	if (!req.secure) {
+    	return next();
+  	};
   //res.redirect('https://localhost:'+HTTPS_PORT+req.url);
-  res.redirect('https://'+req.hostname+':'+HTTPS_PORT+req.url);
+  //res.redirect('https://'+req.hostname+':'+HTTPS_PORT+req.url);
+  res.redirect('http://'+req.hostname+req.url);
 });
 
 //Home
@@ -55,16 +60,17 @@ app.get('/style.css', function(req, res) {
 ///////////////////////////////////////////////////////
 
 var HTTP_PORT = 80;
-var HTTPS_PORT = 443;
+//var HTTPS_PORT = 443;
 
-var options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
+// var options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+//     //pfs: fs.readFileSync('server.p12')
+// };
 
-var secureServer = https.createServer(options, app).listen(HTTPS_PORT, function(){
-  console.log("Secure Express Server listening on port " + HTTPS_PORT);
-});
+// var secureServer = https.createServer(options, app).listen(HTTPS_PORT, function(){
+//   console.log("Secure Express Server listening on port " + HTTPS_PORT);
+// });
 
 var insecureServer = http.createServer(app).listen(HTTP_PORT, function() {
   console.log('Insecure Express Server listening on port ' + HTTP_PORT);
