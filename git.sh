738e2b626fc7a06c1f139738d27143351b618f1c
Xero Studios <parra.justin.main@gmail.com>
2016-07-02 22:48:46 -0600

    Git fix

--- git.sh
+++ git.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # My first script
 
-if [$# > 1]; then
+if ["$#" > 1]; then
 	echo "Too many arguments";
 	exit;
 fi
 
-if [$1 == "pull"]; then
+if ["$1" == "pull"]; then
 	git pull origin master;
 	echo "pulled origin";
 	exit;
 else
-	echo "Git Sh File";
+	echo "==========] Git Sh File [==========";
 	echo "Commands: ";
 	echo " pull";
 fi
